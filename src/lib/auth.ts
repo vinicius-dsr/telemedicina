f8d4915af4034e76a2e459a57d34e2ee03ff2738
Vinícius Reis <viniciusreis3105@gmail.com>
2025-10-10 13:33:06 -0300

    fix: fix lint errors

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -48,13 +48,15 @@ export const authOptions = {
     strategy: 'jwt' as const
   },
   callbacks: {
-    async jwt({ token, user }: { token: any; user: any }) {
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    async jwt({ token, user }: any) {
       if (user) {
         token.role = user.role
       }
       return token
     },
-    async session({ session, token }: { session: any; token: any }) {
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    async session({ session, token }: any) {
       if (token) {
         session.user.id = token.sub!
         session.user.role = token.role as string
